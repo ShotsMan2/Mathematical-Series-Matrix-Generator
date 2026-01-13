59fb842041fa0831edd09708c4b0ab9398a96bbf
Selim Efe <uysalselimefe@gmail.com>
2026-01-13 18:20:21 +0300

    Implement matrix population with mathematical series (n, n^2, n!, n^n)
    Added source code that constructs a 4x10 matrix. The algorithm iterates through integers 1 to 10 and populates rows with linear ($n$), quadratic ($n^2$), factorial ($n!$), and exponential ($n^n$) values. Utilizes 'long long' data types to prevent integer overflow for large calculations.

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+int i,j,carpan;
+
+    long long matris[4][10],sonuc;
+
+    for(i=0;i<10;i++){
+        sonuc=1;
+        carpan=1;
+        matris[0][i]=i+1;
+        matris[1][i]=(i+1)*(i+1);
+        for(j=i+1;j>=1;j--){
+            carpan*=j;
+        }
+        matris[2][i]=carpan;
+        for(j=1;j<=i+1;j++){
+            sonuc=(i+1)*sonuc;
+        }
+        matris[3][i]=sonuc;
+    }
+    for(j=0;j<4;j++){
+        for(i=0;i<10;i++){
+            printf("%lld\t",matris[j][i]);
+        }
+
+        printf("\n");
+    }
+}
